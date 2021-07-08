813f0ca5aeaec539f4bced36a97bb67d772edd60
alejorod62 <alejorodri62@gmail.com>
2021-07-08 15:47:36 -0300

    aver

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, './public')));  
+
 app.get('/', (req,res) =>{
    res.sendFile(path.join(__dirname, './views/home.html'));  // Permite enviar un archivo HTML
 });
 
-app.use(express.static(path.join(__dirname, './public')));  
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/register.html'));  // Permite enviar un archivo HTML
+ });
+
+ app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/login.html'));  // Permite enviar un archivo HTML
+ });
 
 app.listen(3000, () => {
     console.log("Servidor corriendo");
